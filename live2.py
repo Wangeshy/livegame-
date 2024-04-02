09cb45b0af0f922b084939ccb3fcde0b1a14ce8d
wangeshy <136800614+Wangeshy@users.noreply.github.com>
2024-04-03 00:03:21 +0200

    Add files via upload

---
+++ live2.py
@@ -0,0 +1,23 @@
+def welcome(name):
+    print('Hello',name,'and welcome to the World of Games(WoG),\nHere you can find many cool games to play')
+
+welcome(str(input('Please input your name')))
+
+
+
+def load_game():
+    f = {1: 'Memory game - a sequence of numbers will appear for 1 second and you have to guess it back',
+         2: 'Guess Game - guess a number and see if you choose like the computer',
+         3: 'Currency Roulette - try and guess the value of a random amount of USD in ILS'}
+    for i,info in f.items():
+        if (i) == n:
+         print(info)
+        else:
+            pass
+
+n = int(input('Choose the game to play'))
+
+load_game()
+
+
+

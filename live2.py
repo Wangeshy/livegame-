c573156d10701ad5a5fbebf02be4db510463ef55
wangeshy <136800614+Wangeshy@users.noreply.github.com>
2024-04-03 14:40:09 +0200

    Add files via upload

--- live2.py
+++ live2.py
@@ -6,14 +6,11 @@ welcome(str(input('Please input your name')))
 
 
 def load_game():
-    f = {1: 'Memory game - a sequence of numbers will appear for 1 second and you have to guess it back',
+    game_choice = {1: 'Memory game - a sequence of numbers will appear for 1 second and you have to guess it back',
          2: 'Guess Game - guess a number and see if you choose like the computer',
          3: 'Currency Roulette - try and guess the value of a random amount of USD in ILS'}
-    for i,info in f.items():
-        if (i) == n:
-         print(info)
-        else:
-            pass
+
+    print(game_choice[n])
 
 n = int(input('Choose the game to play'))
 
